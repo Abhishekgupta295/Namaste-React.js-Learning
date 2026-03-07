16606f0d219854729672e0f760fc2e81a533436b
Abhishekgupta295 <guptaabhi2952nd@gmail.com>
2026-03-07 20:37:47 +0530

    completed Lec-9, learned about Custom hooks, lazy loading

--- Hungers_Hubb/src/App.jsx
+++ Hungers_Hubb/src/App.jsx
@@ -3,13 +3,26 @@ import './App.css'
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import About from './components/About';
+//import About from './components/About';
 import Error from './components/Error';
 import Contact from './components/Contact';
 import RestaurantMenu from './components/RestaurantMenu';
 import Profile from './components/Profile';
 import ProfileClass from './components/ProfileClass';
 import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { lazy,Suspense } from 'react';
+import Shimmer from './components/Shimmer';
+//import Instamart from './components/Instamart';
+
+// dynamic import for code splitting and lazy loading of Instamart component
+const Instamart = lazy(() => import('./components/Instamart'));
+const About = lazy(() => import('./components/About')); 
+//this is a promise which resolves to a module with default export as Instamart component
+// Never lazy load a compoent inside other component body or inside useEffect or any other function body, 
+// it should be at the top level of the file only, otherwise it will not work and will throw error "Invalid hook call.
+//  Hooks can only be called inside of the body of a function component." because lazy is a hook and it can only be called 
+// at the top level of the file, if we call it inside other function body then it will throw error because it will violate 
+// the rules of hooks.
 
  const AppLayout = () =>
 {
@@ -37,7 +50,7 @@ const appRouter = createBrowserRouter(
                         },
                         {
                           path : "/About",
-                          element : <About />,
+                          element : <Suspense fallback = {<h1>Loading About...</h1>}><About/></Suspense>,
                           children : [
                             {
                               path : "profile", // nested route path needs no leading slash => parentPath/{path} -> localhost:1234/About/profile
@@ -58,6 +71,12 @@ const appRouter = createBrowserRouter(
                         {
                           path : "/Restaurant/:id",
                           element : <RestaurantMenu/>,
+
+                        },
+                        {
+                          path : "/instamart",
+                          element : <suspense fallback = {<h1>Loading Instamart...</h1>}><Instamart/></suspense>, // Instamart component will be loaded only when user
+                          //  navigates to /instamart route
 
                         },
                    ]
