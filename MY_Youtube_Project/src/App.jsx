10bc9edf818ebe7e7a3714a0115c0545a6e5b9a9
Abhishekgupta295 <guptaabhi2952nd@gmail.com>
2026-04-15 20:39:41 +0530

    Started Youtube Project

---
+++ MY_Youtube_Project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-600 text-yellow-300'>HI Namaste Youtube!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
